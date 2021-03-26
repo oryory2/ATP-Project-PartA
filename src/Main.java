abaf74e33908333ae6f81bd00e0221fac88bffff
Or izhaky <oryory2@gmail.com>
2021-03-26 15:03:22 +0300

    firstCommit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hey");
+        System.out.println("dd");
+        System.out.println("dfdsf");
+    }
+}
