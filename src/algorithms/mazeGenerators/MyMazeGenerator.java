1ca08a87bcb7d9d043ac0cc32d7224240cb245d7
Or izhaky <oryory2@gmail.com>
2021-03-26 19:40:25 +0300

    fourth

--- src/algorithms/mazeGenerators/MyMazeGenerator.java
+++ src/algorithms/mazeGenerators/MyMazeGenerator.java
@@ -1,5 +1,7 @@
 package algorithms.mazeGenerators;
 
+import java.sql.PseudoColumnUsage;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,31 +9,41 @@ public class MyMazeGenerator extends AMazeGenerator
 {
     public Maze generate(int row, int column) {
         int[][] mazeArr = new int[row][column];
-        for (int i = 0; i <= mazeArr.length; i++) {
+        for (int i = 0; i <= mazeArr.length; i++)
+        {
             for (int j = 0; j <= mazeArr[0].length; j++)
                 mazeArr[i][j] = 1;
         }
         mazeArr[0][0] = 0;
         Position thisPose = new Position(0,0);
-        Set<String> wallSet = new HashSet<String>();
-
+        ArrayList<Position> PoseList = new ArrayList<Position>();
         do
             {
-
-
-
+                Position [] legalNeighbors = Position.findLegalNeighbors(thisPose, thisPose.getRow(), thisPose.getColumn());
+                for(int i = 0; i < 4; i++)
+                {
+                    if(legalNeighbors[i] == null)
+                        break;
+                    if(mazeArr[legalNeighbors[i].getRow()][legalNeighbors[i].getColumn()] == 1)
+                        PoseList.add(legalNeighbors[i]);
+                }
+                int flag = 0;
+                int randomPick = (int) (Math.random() * PoseList.size());
+                Position p = PoseList.get(randomPick);
+                if((p.getRow() == mazeArr.length - 1) && (p.getColumn() == mazeArr[0].length - 1))
+                {
+                    mazeArr[mazeArr.length][mazeArr[0].length] = 0;
+                    Maze newMaze = new Maze(mazeArr);
+                    return newMaze;
+                }
+                else
+                {
+                    thisPose = p;
+                    PoseList.remove(p);
+                    mazeArr[thisPose.getRow()][thisPose.getColumn()] = 0;
+                }
         }
-        while(!(wallSet.isEmpty()));
-
-
-
-
-
-
-
-
-
-
+        while(!(PoseList.isEmpty()));
         Maze newMaze = new Maze(mazeArr);
         return newMaze;
     }
