66c9fb95265ff975e349e00ce68704c436e5861a
adamsha123 <adamsha@post.bgu.ac.il>
2021-04-07 16:42:20 +0300

    Maze rewind

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,9 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("hey");
-        MyMaze3DGenerator m = new MyMaze3DGenerator();
-        Maze3D maze = m.generate(100,100,100);
-        SearchableMaze3D Smaze = new SearchableMaze3D(maze);
+        MyMazeGenerator m = new MyMazeGenerator();
+        Maze maze = m.generate(9,9);
+        SearchableMaze Smaze = new SearchableMaze(maze);
         DepthFirstSearch d = new DepthFirstSearch();
         Solution s1 = d.solve(Smaze);
         //BreadthFirstSearch b = new BreadthFirstSearch();
