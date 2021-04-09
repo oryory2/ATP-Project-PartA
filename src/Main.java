eeb1de8b790ab7c464c241f5a1059a30816c239b
Or izhaky <oryory2@gmail.com>
2021-04-09 21:41:27 +0300

    Working

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import algorithms.maze3D.IMazeGenerator3D;
+import algorithms.maze3D.IMaze3DGenerator;
 import algorithms.maze3D.Maze3D;
 import algorithms.maze3D.MyMaze3DGenerator;
 import algorithms.maze3D.SearchableMaze3D;
@@ -22,7 +22,7 @@ public class Main
         //Solution s2 = b.solve(Smaze);
         //BestFirstSearch BF = new BestFirstSearch();
         //Solution s3 = BF.solve(Smaze);
-        IMazeGenerator3D g = new MyMaze3DGenerator();
+        IMaze3DGenerator g = new MyMaze3DGenerator();
         Maze3D newMaze = g.generate(2,2, 2);
         SearchableMaze3D sMaze = new SearchableMaze3D(newMaze);
         BreadthFirstSearch bfs = new BreadthFirstSearch();
