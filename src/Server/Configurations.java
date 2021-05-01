b6e670aec34c53ad6f9d4e3503a9086496bf6d55
adamsha123 <adamsha@post.bgu.ac.il>
2021-05-01 18:29:38 +0300

    configurations

--- src/Server/Configurations.java
+++ src/Server/Configurations.java
@@ -1,9 +1,7 @@
 package Server;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.Properties;
 
 
 public class Configurations
@@ -19,6 +17,42 @@ public class Configurations
             Configurations.instance = new Configurations();
         }
         return Configurations.instance;
+    }
+
+    public static void writeProp(String NumberOfThreads , String MGA  ,String MSA ){
+        try (OutputStream output = new FileOutputStream("config.properties.txt")) {
+
+            Properties prop = new Properties();
+            // set the properties value
+            prop.setProperty("threadPoolSize", NumberOfThreads);
+            prop.setProperty("mazeGeneratingAlgorithm", MGA);
+            prop.setProperty("mazeSearchingAlgorithm", MSA);
+
+            // save properties to project root folder
+            prop.store(output, null);
+
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+
+    public static String[] LoadProp(){
+        try (InputStream input = new FileInputStream("config.properties.txt")) {
+
+            Properties prop = new Properties();
+
+            // load a properties file
+            prop.load(input);
+            String[] PropArr = new String[3];
+            PropArr[0] = prop.getProperty("threadPoolSize");
+            PropArr[1] = prop.getProperty("mazeGeneratingAlgorithm");
+            PropArr[2] = prop.getProperty("mazeSearchingAlgorithm");
+            return PropArr;
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
 
 }
